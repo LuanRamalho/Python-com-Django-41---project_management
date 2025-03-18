36652b2dccdec8daf48d1aef61692cb5e8a7d8fd
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-18 16:40:24 -0300

    urls.py

--- Python com Django 41 - project_management/tasks/urls.py
+++ Python com Django 41 - project_management/tasks/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('project/<int:project_id>/add_task/', views.add_task, name='add_task'),
     path('team/<int:team_id>/add_project/', views.add_project, name='add_project'),
     path('add_team/', views.add_team, name='add_team'),
+    path('team/<int:team_id>/edit/', views.edit_team, name='edit_team'),
+    path('project/<int:project_id>/delete/', views.delete_team, name='delete_team'),
+    path('project/<int:project_id>/edit/', views.edit_project, name='edit_project'),  
+    path('project/<int:project_id>/confirm_delete/', views.confirm_delete_project, name='confirm_delete_project'),
+    path('task/<int:task_id>/edit/', views.edit_task, name='edit_task'),
+    path('task/<int:task_id>/delete/', views.delete_task, name='delete_task'),
+
 ]
